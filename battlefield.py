cfaec783424c42a7d0e8f46340f02573f98ed743
rahk1990 <rahkem.ellis@gmail.com>
2022-02-03 15:12:32 -0500

    Added instinces to following files; battlefield class and main.

--- battlefield.py
+++ battlefield.py
@@ -1,7 +1,33 @@
 from fleet import Fleet
+from herd import Herd
 
 
 
 
 class Battlefield:
 
+    def __init__(self):
+        self.fleet = Fleet
+        self.herd = Herd
+
+    def run_game(self):
+        pass
+    
+    def welcome(self):
+        print('Welcome to Game!')
+        pass
+    
+    def battle(self):
+        pass
+    
+    def dino_turn(self):
+        pass
+
+    def robo_turn(self):
+        pass
+
+    def show_dino_opponent_options(self):
+        pass
+
+    def display_winners(self):
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from battlefield import Battlefield
+
+
+run_game
